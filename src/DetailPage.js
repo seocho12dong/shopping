d7a433f0aece4e4e26340940626e2ab1f0da33ba
ryu <ghuh243@naver.com>
2024-07-31 14:44:21 +0900

    url 파라미터 과제 끝

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -3,30 +3,26 @@ import data from "./data";
 
 function DetailPage(props) {
 
-    let {id} = useParams();
-    let temp;
-
-    data.map(function (a, i){
-        if (props.shoes[i].id === id) {
-            temp = props.shoes[i].id;
-        }
-    })
+    let { id } = useParams();
+    let 찾은상품 = props.shoes.find(function(x){
+        return x.id == id
+    });
 
     return (
         <div className="container">
             <div className="row">
                 <div className="col-md-6">
-                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
+                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
                 </div>
-                <div className="col-md-6">
-                    <h4 className="pt-5">{props.shoes[temp].title}</h4>
-                    <p>{props.shoes[temp].content}</p>
-                    <p>{props.shoes[temp].price}</p>
+                <div className="col-md-6 mt-4">
+                    <h4 className="pt-5">{찾은상품.title}</h4>
+                    <p>{찾은상품.content}</p>
+                    <p>{찾은상품.price}원</p>
                     <button className="btn btn-danger">주문하기</button>
                 </div>
             </div>
         </div>
     )
-}
+};
 
-export default DetailPage;
+export default DetailPage
