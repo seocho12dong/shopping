ce984aafe0a4cd663381a9155030c7ce805f912a
ryu <ghuh243@naver.com>
2024-08-03 16:25:47 +0900

    GPT쓴 lifecycle 풀기

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -1,33 +1,31 @@
-import {useParams} from "react-router-dom";
-import styled from 'styled-components'
-import {useEffect, useState} from "react";
-
+import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+import { useEffect, useState } from "react";
 
 function DetailPage(props) {
-    useEffect(()=> {
-        setInterval(()=>{
-            setShow(false)
-        },2000)
-    }, [])
-
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShow(false);
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, []);
 
     let { id } = useParams();
-    let 찾은상품 = props.shoes.find(function(x){
-        return x.id == id
+    let 찾은상품 = props.shoes.find(function (x) {
+        return x.id == id;
     });
-    const [show, setShow] = useState(true)
+    const [show, setShow] = useState(true);
+    const [idValue, setIdValue] = useState('');
 
-    const [idValue, setIdValue] = useState(0)
     const handleInputChange = (event) => {
         setIdValue(event.target.value);
     };
-
 
     let YellowBtn = styled.button`
         background: yellow;
         color : black;
         padding : 10px;
-    `
+    `;
 
     return (
         <div className="container">
@@ -38,11 +36,11 @@ function DetailPage(props) {
             )}
             <div className="row">
                 <div className="col-md-6">
-                    <img src={`https://codingapple1.github.io/shop/shoes${parseInt(id)+1}.jpg`} width="100%"/>
+                    <img src={`https://codingapple1.github.io/shop/shoes${parseInt(id) + 1}.jpg`} width="100%" />
                 </div>
                 <div className="col-md-6 mt-4">
                     <YellowBtn>버튼</YellowBtn>
-                    <InputTest setIdValue={setIdValue} IdValue={idValue} handleInputChange={handleInputChange}></InputTest>
+                    <InputTest setIdValue={setIdValue} idValue={idValue} handleInputChange={handleInputChange}></InputTest>
                     <h4 className="pt-5">{찾은상품.title}</h4>
                     <p>{찾은상품.content}</p>
                     <p>{찾은상품.price}원</p>
@@ -50,23 +48,22 @@ function DetailPage(props) {
                 </div>
             </div>
         </div>
-    )
-};
-
-function InputTest(props) {
-    useEffect(()=> {
-        if (isNaN(props.IdValue) === true) {
-            alert('숫자를 입력해주세요')
-            props.setIdValue(0)
-        }
-        else console.log('숫자를 입력했네요')
-        return() => {
-            props.setIdValue(0)
-        }
-    },  [props.IdValue])
-    return (
-        <input value={ props.setIdValue(props.IdValue) } onChange={ props.handleInputChange }/>
-    )
+    );
 }
 
-export default DetailPage
+function InputTest(props) {
+    useEffect(() => {
+        if (isNaN(props.idValue)) {
+            alert('숫자를 입력해주세요');
+            props.setIdValue('');
+        } else {
+            console.log('숫자를 입력했네요');
+        }
+    }, [props.idValue]); // props.idValue가 변경될 때만 실행
+
+    return (
+        <input value={props.idValue} onChange={props.handleInputChange} />
+    );
+}
+
+export default DetailPage;
